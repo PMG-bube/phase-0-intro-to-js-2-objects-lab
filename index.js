acfdf01975eed1b06b6ace532cea78babcb3aaf5
pmg <gachigipm@gmail.com>
2024-10-03 09:22:28 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+const employee={
+    name:"Sam",
+    streetAddress:"2526",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+return {...employee,
+    [key]:value};
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+ employee.streetAddress='12 Broadway'
+return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+const delf={...employee};
+delete delf[key]
+return delf;
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+  delete employee[key]
+return employee;
+}
